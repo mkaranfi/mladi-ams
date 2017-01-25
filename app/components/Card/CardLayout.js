1b889147380e726454c04d7eae85c4ee45a8786a
Crestafore <milewow1@gmail.com>
2017-01-25 23:56:33 +0100

    Add local async storage for offline usage

--- app/components/Card/CardLayout.js
+++ app/components/Card/CardLayout.js
@@ -2,7 +2,7 @@
  * Created by Mile on 24-Jan-17.
  */
 import React, {Component} from 'react';
-import {ListView, View} from 'react-native';
+import {ListView, View, AsyncStorage} from 'react-native';
 
 import Card from './Card';
 import styles from './styles';
@@ -22,28 +22,38 @@ class CardLayout extends Component {
         };
     }
 
+    manageDataFromAPI(data, scope) {
+        // Conference || Seminar
+        //scope.filterThroughArray(data, listingTypes.CONFERENCE, 'Conference');
+
+        // Internship || Job
+        // data = scope.filterThroughArray(data, listingTypes.JOB, 'Job');
+
+        scope.setState({
+            dataSource: scope.ds.cloneWithRows(data)
+        });
+    }
+
     componentWillMount() {
         let thisClassScoped = this;
-        this.fetchData(SCHOLLARSHIP_API).then(function (data) {
-            // Conference || Seminar
-            //thisClassScoped.filterThroughArray(data, listingTypes.CONFERENCE, 'Conference');
-
-            // Internship || Job
-            // data = thisClassScoped.filterThroughArray(data, listingTypes.JOB, 'Job');
-
-            thisClassScoped.setState({
-                dataSource: thisClassScoped.ds.cloneWithRows(data)
+        this.fetchData(TRAININGS_API).then(function (data) {
+            AsyncStorage.setItem('cardData', JSON.stringify(data));
+            thisClassScoped.manageDataFromAPI(data, thisClassScoped);
+        })
+            .catch((err) => {
+                AsyncStorage.getItem('cardData')
+                    .then((data) => JSON.parse(data))
+                    .then((data) => {
+                        data !== null ? thisClassScoped.manageDataFromAPI(data, thisClassScoped) :
+                            console.error('Error: ' + err);
+                    });
             });
-        }).catch((err) => console.error('error=>' + err));
     }
 
     fetchData(query) {
         return fetch(query)
             .then((response) => response.json())
             .then((data) => data)
-            .catch(function (err) {
-                console.error('error => ' + err);
-            });
     }
 
     filterThroughArray(array, type, keyword) {
@@ -73,7 +83,7 @@ class CardLayout extends Component {
         let date = rowData.CrawlDate === undefined ? rowData.Inserted : rowData.CrawlDate;
 
         return (<Card {...this.props} title={title} site={source} date={date}
-                                      description={rowData.Description} url={rowData.Link}/>);
+                      description={rowData.Description} url={rowData.Link}/>);
     }
 
 }
